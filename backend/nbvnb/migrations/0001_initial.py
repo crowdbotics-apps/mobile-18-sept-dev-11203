e2dbf6f9632f29e6b6d290c3ab8eb3d4583c0a2f
Crowdbotics <team@crowdbotics.com>
2020-09-18 07:01:51 +0000

    Update models

--- backend/nbvnb/migrations/0001_initial.py
+++ backend/nbvnb/migrations/0001_initial.py
@@ -7,17 +7,27 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Bngvnbvnb',
+            name="Bngvnbvnb",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('jkjhkhjhk', models.BigIntegerField()),
-                ('ngghgjhgjhg', models.GenericIPAddressField(protocol='IPv4')),
-                ('nbmbnmnbmnb', models.DecimalField(decimal_places=100, max_digits=300)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("jkjhkhjhk", models.BigIntegerField()),
+                ("ngghgjhgjhg", models.GenericIPAddressField(protocol="IPv4")),
+                (
+                    "nbmbnmnbmnb",
+                    models.DecimalField(decimal_places=100, max_digits=300),
+                ),
             ],
         ),
     ]
--- backend/nbvnb/models.py
+++ backend/nbvnb/models.py
@@ -5,14 +5,6 @@ from django.db import models
 class Bngvnbvnb(models.Model):
     "Generated Model"
     jkjhkhjhk = models.BigIntegerField()
-    ngghgjhgjhg = models.GenericIPAddressField(
-        protocol="IPv4",
-        unpack_ipv4=False,
-    )
-    nbmbnmnbmnb = models.DecimalField(
-        max_digits=300,
-        decimal_places=100,
-    )
 
 
 # Create your models here.
